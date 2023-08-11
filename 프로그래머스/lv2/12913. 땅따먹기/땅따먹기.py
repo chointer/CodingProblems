5340b3bb47539d8ebb82567a8afb5c07642647f6
chointer <77265724+chointer@users.noreply.github.com>
2023-08-11 17:26:24 +0900

    [level 2] Title: 땅따먹기, Time: 132.44 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,43 +1,5 @@
-# remember 1st and 2nd plans and their final positions
-# list.index(value), list.pop(idx)
-
-def find(row):
-    row_sort = sorted(row, reverse=True)
-    val1 = row_sort[0]
-    val2 = row_sort[1]
-    val3 = row_sort[2]
-    pos1, pos2, pos3 = -1, -1, -1
-    
-    for i, num in enumerate(row):
-        if pos1 == -1 and num == val1: pos1 = i
-        elif pos2 == -1 and num == val2: pos2 = i
-        elif num == val3: pos3 = i
-    
-    return val1, pos1, val2, pos2, val3, pos3
-
-
+# calcuate the max for each pos.
 def solution(land):
-    """
-    t_val1, t_pos1, t_val2, t_pos2, t_val3, t_pos3 = find(land.pop(0))
-    
-    for r in land:
-        val1, pos1, val2, pos2, val3, pos3 = find(r)
-        if t_pos1 != pos1:
-            # for 2nd
-            candidates = []
-            if t_pos1 != pos2: candidates.append((t_val1 + val2, pos2))
-            if t_pos2 != pos2: candidates.append((t_val2 + val2, pos2))
-            if t_pos1 != pos3: candidates.append((t_val1 + val3, pos3))
-            t_val1 += val1
-            t_pos1 = pos1
-            t_val2, t_pos2 = max(candidates)
-        else:
-            # 1st
-            candidates = [(t_val1 + val2, pos2), (t_val2 + val1, pos1)]
-            candidates.sort(reverse=True)
-            t_val1, t_pos1 = candidates[0]
-            t_val2, t_pos2 = candidates[1]
-    """
     memory = [0, 0, 0, 0]
     for r in land:
         memory_temp = [0, 0, 0, 0]
