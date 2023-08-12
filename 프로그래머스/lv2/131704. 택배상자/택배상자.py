b3c9bd4d531898b0163b650c8dee4289ff137548
chointer <77265724+chointer@users.noreply.github.com>
2023-08-12 15:10:14 +0900

    [level 2] Title: 택배상자, Time: 304.68 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(order):
+    N = len(order)
+    order = deque(order)
+    boxes = deque(range(1, len(order) + 1))
+    saver = deque([])
+    saver_size = 0
+    
+    while order:
+        if boxes and order[0] == boxes[0]:
+            order.popleft()
+            boxes.popleft()
+        elif saver and order[0] == saver[0]:
+            order.popleft()
+            saver.popleft()
+        elif boxes and saver_size < 5:
+            saver.appendleft(boxes.popleft())
+        else:
+            break
+    
+    return N - len(order)
