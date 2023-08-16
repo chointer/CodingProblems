3f5dc4f80090c374f4a4e8c7c51ee386f6ad0fc8
chointer <77265724+chointer@users.noreply.github.com>
2023-08-16 19:12:50 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+def solution(fees, records):
+    # stack
+    T_END = 23*60 + 59
+    TimeBook = {}
+    StackBook = defaultdict(int)
+    
+    for record in records:
+        t, car_num, action = record.split()
+        t = int(t[:2])*60 + int(t[3:])
+        if action == "IN":
+            TimeBook[car_num] = t
+        else:  # "OUT"
+            StackBook[car_num] += t - TimeBook[car_num]
+            del TimeBook[car_num]
+    
+    for car_num, t in TimeBook.items():
+        StackBook[car_num] += T_END - t
+        
+    # calculate
+    answer = []
+    time_min, fee_min, time_add, fee_add = fees
+    car_nums = sorted(StackBook.keys())
+    
+    for car_num in car_nums:
+        t = StackBook[car_num]
+        if t <= time_min:
+            answer.append(fee_min)
+            continue
+        else:
+            t = t - time_min
+            fee = fee_min + (t//time_add) * fee_add
+            if t%time_add != 0:
+                fee += fee_add
+            answer.append(fee)
+    
+    return answer
