dd1e7b84b4f6e03de7380c12f354d8146d4d8bae
chointer <77265724+chointer@users.noreply.github.com>
2023-08-18 15:18:44 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 1241.95 ms, Memory: 219 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -0,0 +1,18 @@
+# 모르겠어서 단순하게 탐색 -> 시간 초과
+# 힌트 참고 (DP) -> 시간 초과
+# 거의 정답 참고 (2번째로 구현했던 것이 DP는 아니었던 것 같다.)
+# 
+
+def square(note1, note2, note3, val):
+    maxlen_now = min(note1[1], note2[1], note3[1]) + 1 if val == 1 else 0
+    maxlen = max(note1[0], note2[0], note3[0], maxlen_now)
+    return (maxlen, maxlen_now)
+
+from collections import defaultdict
+def solution(board):
+    notes = defaultdict(lambda:(0, 0))
+
+    for y, bo in enumerate(board):
+        for x, val in enumerate(bo):
+            notes[(y, x)] = square(notes[(y-1, x-1)], notes[(y-1, x)], notes[(y, x-1)], val)
+    return notes[(y, x)][0]**2
