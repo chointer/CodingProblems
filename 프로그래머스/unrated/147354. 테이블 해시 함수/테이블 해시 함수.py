87d45b5f8836aaef21d9e3420e395cb29d7d1090
chointer <77265724+chointer@users.noreply.github.com>
2023-08-20 16:25:49 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,16 @@
+# sort lambda, bitwise XOR
+# 2,500 * 500 = 1,250,000
+
+def solution(data, col, row_begin, row_end):
+    # 1 & 2 (Sorting)
+    data.sort(key=lambda x: (x[col-1], -x[0]))
+
+    # 3 S_is
+    Si0 = 0
+    for i, row in enumerate(data[row_begin - 1: row_end], start=row_begin):
+        Si = 0
+        for val in row:
+            Si += val%i
+        Si0 = Si ^ Si0
+
+    return Si0
