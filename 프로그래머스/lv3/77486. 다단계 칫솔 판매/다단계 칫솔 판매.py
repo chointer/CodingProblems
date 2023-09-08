96b7f8d1a100a85c45f5648c70f9d0d036246c9b
chointer <77265724+chointer@users.noreply.github.com>
2023-09-08 21:15:28 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 154.04 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,6 +1,6 @@
 # 최하위 멤버(계산이 끝난) 계산, 추천인에게 돈 전달.
 # 1차. 31m 틀림. seller, amount에 여러 번 들어가면, 총합을 계산하면 안 되고, 나누어 계산해야 한다. 원단위 절사 과정이 달라지기 때문
-# 2차. 1e5 * 1e4 -> 1e9 안될 것 같은데...
+# 2차. 1e5 * 1e4 -> 1e9 안될 것 같은데... 되네
 
 def solution(enroll, referral, seller, amount):
     profits = {mem:0 for mem in enroll}
@@ -16,4 +16,5 @@ def solution(enroll, referral, seller, amount):
             money = money_give
             sell = refer_dict[sell]
 
-    return [profits[mem] for mem in enroll]
+    answer = [profits[mem] for mem in enroll]
+    return answer
