82d638ebe4e4fab1e9bb9d2cd61577f76c01665b
chointer <77265724+chointer@users.noreply.github.com>
2023-11-21 11:57:24 +0900

    [level 2] Title: 롤케이크 자르기, Time: 241.19 ms, Memory: 51.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,26 @@
+# topping <= 1e6
+# 6m 14s
+from collections import Counter
+
+def solution(topping):
+    me = Counter()
+    bro = Counter(topping)
+    n_me = 0
+    n_bro = len(bro.keys())
+    
+    answer = 0
+    for topp in topping:
+        me[topp] += 1
+        if me[topp] == 1:
+            n_me += 1
+        
+        bro[topp] -= 1
+        if bro[topp] == 0:
+            n_bro -= 1
+            
+        if n_me == n_bro:
+            answer += 1
+        elif n_me > n_bro:
+            break
+    
+    return answer
