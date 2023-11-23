39a5b37225ba7c7d918798263842936ffea15fbc
chointer <77265724+chointer@users.noreply.github.com>
2023-11-23 18:28:05 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,14 @@
+# arr <= 100, commands <= 50
+# cmd * arr*log(arr) ~ 50*100*2 = 1e5
+
+def find(subarr, k):
+    return sorted(subarr)[k-1]
+
+
+def solution(array, commands):
+    answer = []
+    
+    for st, en, k in commands:
+        answer.append( sorted(array[st - 1 : en])[k-1] )
+
+    return answer
