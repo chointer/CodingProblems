5e093aeaf89e4c27c54ce956661f772a62872d5b
chointer <77265724+chointer@users.noreply.github.com>
2024-01-18 18:41:41 +0900

    [level 2] Title: 주식가격, Time: 71.00 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -2,6 +2,8 @@
 # 주제가 stack & queue. => 흠. 모르겠다. 
 # 순서대로 체크(stack)하고 트리거가 되면 일괄 처리(pop) 같은데
 # heapq?
+# 풀이를 봤는데, heapq 대신 stack으로 해도 된다. 어차피 뒤 순서대로 처리할 것을 빼내다보면, 처리할 것만 남아있다. 항상 i번째 요소는 그 다음의 모든 요소보다 작거나 같다.
+
 import heapq
 
 def solution(prices):
