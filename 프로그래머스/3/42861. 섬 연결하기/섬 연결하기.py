cd58d8174759993ac92ad0874ce0a3b2fc754791
chointer <77265724+chointer@users.noreply.github.com>
2024-03-08 20:01:00 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,35 @@
+# N <= 100. costs < 5000
+# parent가 다르면 union & 건설
+# parent가 같으면 pass
+
+def find(x, parents):
+    if parents[x] == x:
+        return x
+    
+    parents[x] = find(parents[x], parents)
+    return parents[x]
+
+def union(a, b, parents):
+    pa = find(a, parents)
+    pb = find(b, parents)
+    if pa < pb:
+        parents[pb] = pa
+        return True
+    elif pa > pb:
+        parents[pa] = pb
+        return True
+    else:
+        return False
+        
+        
+def solution(n, costs):
+    parents = [i for i in range(n + 1)]
+    costs = sorted(costs, key=lambda x: x[2])
+    answer = 0
+    
+    for a, b, cost in costs:
+        unioned = union(a, b, parents)
+        if unioned:
+            answer += cost
+    
+    return answer
