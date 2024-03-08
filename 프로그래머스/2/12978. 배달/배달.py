b574bdce7db2305df35ec004b138e36139478d70
chointer <77265724+chointer@users.noreply.github.com>
2024-03-08 18:21:55 +0900

    [level 2] Title: 배달, Time: 20.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,45 +1,25 @@
-# N <= 50, roads <= 2000
-# Dijkstra O( (V+E) log V ) < 1e4?
+# Floyd-Warshall 연습.
 
-import heapq
 def solution(N, road, K):
-    roads = [[] for i in range(N)]
-    
-    for (a, b, cost) in road:
-        a -= 1
-        b -= 1
+    inf = 100000000
+    roads = [[inf for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        roads[i][i] = 0
         
-        for idx, ra in enumerate(roads[a]):
-            if ra[0] == b:
-                if ra[1] > cost:
-                    roads[a][idx][1] = cost
-                break
-        else:
-            roads[a].append([b, cost])
-            
-        for idx, rb in enumerate(roads[b]):
-            if rb[0] == a:
-                if rb[1] > cost:
-                    roads[b][idx][1] = cost
-                break
-        else:
-            roads[b].append([a, cost])
+    for a, b, c in road:
+        if roads[a - 1][b - 1] > c:
+            roads[a - 1][b - 1] = c
+            roads[b - 1][a - 1] = c
+        
+    # Floyd-Warshall
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                if roads[i][j] > roads[i][k] + roads[k][j]:
+                    roads[i][j] = roads[i][k] + roads[k][j]
 
-    
-    INF = 100000000
-    costs = [INF for i in range(N)]
-    hq = [[0, 0]]
-    costs[0] = 0
-    
-    while hq:
-        print(hq)
-        cost, loc = heapq.heappop(hq)
-        if costs[loc] < cost:
-            continue
-        for des in roads[loc]:
-            new_cost = cost + des[1]
-            if costs[des[0]] > new_cost:
-                costs[des[0]] = new_cost
-                heapq.heappush(hq, [new_cost, des[0]])
-    
-    return len([c for c in costs if c <= K])
+    count = 0
+    for c in roads[0]:
+        if c <= K:
+            count += 1
+    return count
