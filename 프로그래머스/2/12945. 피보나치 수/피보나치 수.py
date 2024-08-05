c7bfad2ab341c8432e8eab2bd66ede1d8b0e113f
chointer <chointer04@gmail.com>
2024-08-05 18:19:34 +0900

    [level 2] Title: 피보나치 수, Time: 352.42 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,25 @@
+
+"""
+n 번째 피보나치 수를 구하려면,
+
+for i in range(?):    2, 3, ..., n
+    f(i) = f(i-2) + f(i-1)
+    
+f(2) = f(0) + f(1)
+f(3) = f(1) + f(2)
+f(4) = f(2) + f(3)
+...
+
+
+"""
+
+
 def solution(n):
-    fi0, fi1 = 1, 1
+    seq = [0, 1]
     
-    for i in range(n - 2):
-        fi0, fi1 = fi1, (fi0 + fi1)%1234567
-    
-    return fi1
+    for i in range(n-1):
+        j = i + 2
+        value = seq[j-2] + seq[j-1]
+        seq.append(value)
+
+    return seq[n]%1234567
