d8189386df214b39c5a0d5b66b57dcdc61c5f96a
chointer <chointer04@gmail.com>
2024-08-08 13:05:17 +0900

    [level 2] Title: 주식가격, Time: 72.50 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,24 +1,22 @@
-# length <= 1e6, val <= 1e5
-# 주제가 stack & queue. => 흠. 모르겠다. 
-# 순서대로 체크(stack)하고 트리거가 되면 일괄 처리(pop) 같은데
-# heapq?
-# 풀이를 봤는데, heapq 대신 stack으로 해도 된다. 어차피 뒤 순서대로 처리할 것을 빼내다보면, 처리할 것만 남아있다. 항상 i번째 요소는 그 다음의 모든 요소보다 작거나 같다.
+# N <= 1e5
+# price 내림차순으로 기록(heapq?)하고, t시점에서, price보다 높은 과거 기록은 모두 갱신
+# 마지막에 나머지들 후처리?
 
 import heapq
-
 def solution(prices):
-    length = len(prices)
+    answers = [-1 for i in range(len(prices))]
     hq = []
-    answer = [-1 for _ in range(length)]
     
-    for t, price in enumerate(prices):
-        heapq.heappush(hq, (-price, t))
-        while hq and hq[0][0] < -price:
-            _, idx = heapq.heappop(hq)
-            answer[idx] = t - idx
+    for t, p in enumerate(prices):
+        while hq and -hq[0][0] > p:
+            pp, tt = heapq.heappop(hq)
+            answers[tt] = t - tt
+        
+        heapq.heappush(hq, (-p, t))
     
-    
-    for i, val in enumerate(answer):
+    T = len(prices) - 1
+    for i, val in enumerate(answers):
         if val == -1:
-            answer[i] = length - i - 1
-    return answer
+            answers[i] = T - i
+    
+    return answers
