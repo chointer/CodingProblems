0baa2bfa35d804677ef16e3ae3778081bc1d4c56
chointer <chointer04@gmail.com>
2025-03-18 21:26:20 +0900

    [level 2] Title: 타겟 넘버, Time: 1723.20 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,36 +1,11 @@
-# 2**20 ~ 10**6
-# DFS
+from itertools import product
 def solution(numbers, target):
-    # (now, list_left)
     count = 0
-    stack = [(0, numbers)]
-
-    while stack:
-        now, lefts = stack.pop()
-        if not lefts:   # end case
-            if now == target:
-                count += 1
-            else:
-                continue
-        
-        else:
-            stack.append((now + lefts[0], lefts[1:]))
-            stack.append((now - lefts[0], lefts[1:]))
-    return count
-
-
-
-
-
-
-
-"""
-def func(nums, tar, val, idx):
-    if idx == -1:
-        return 1 if val == tar else 0
-    
-    return func(nums, tar, val+nums[idx], idx - 1) + func(nums, tar, val-nums[idx], idx - 1)
+    for opers in product(*[[1, -1] for _ in range(len(numbers))]):
+        temp_sum = 0
+        for oper, num in zip(opers, numbers):
+            temp_sum += oper * num
+        if temp_sum == target:
+            count += 1
             
-def solution(numbers, target):
-    return func(numbers, target, 0, len(numbers)-1)
-"""
+    return count
