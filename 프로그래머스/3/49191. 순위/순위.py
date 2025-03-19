7e255627a3a1fe435dd08a5819dc34ad2cfeb5a8
chointer <chointer04@gmail.com>
2025-03-19 23:32:14 +0900

    [level 3] Title: 순위, Time: 27.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,32 +1,41 @@
-# n <= 100, matches <= 4500
-# 각 선수에 대해 계산해도 시간이 부족하지 않을 것 같다.
-
-def BFS(player, graph, n):
-    count = 0
-    lis = [player]
-    visits = [False for _ in range(n + 1)]
-    # visits[player] = True
-    
-    while lis:
-        player = lis.pop()
-        for p in graph[player]:
-            if not visits[p]:
-                count += 1
-                visits[p] = True
-                lis.append(p)
-    
-    return count
-
+# 어떤 선수는 직, 간접적으로 승패가 확인되면 순위가 결정된다.
+# 간접을 어떻게 모두 체크할까.
+# lose map, win map으로 하면 되려나.
+# for 선수, lose 방향, win 방향으로 순회하면서 기록하기. => 100^2
+# 더 효율적인 방법이 있을 것 같은데..
 
 def solution(n, results):
-    wins = {i + 1: [] for i in range(n)}
-    loses = {i + 1: [] for i in range(n)}
-    for winner, loser in results:
-        wins[winner].append(loser)
-        loses[loser].append(winner)
+    lose_map = [[] for _ in range(n)]   # lose_map[i] = [a, b, c]. i가 a, b, c에게 졌다.
+    win_map = [[] for _ in range(n)]    # win_map[i] = [a, b, c]. i가 a, b, c에게 이겼다.
+    for result in results:
+        winner, loser = result
+        winner -= 1
+        loser -= 1
+        lose_map[loser].append(winner)
+        win_map[winner].append(loser)
     
-    answer = 0
-    for i in range(1, n + 1):
-        if BFS(i, wins, n) + BFS(i, loses, n) == n - 1:
-            answer += 1
-    return answer
+    determined_map = [set() for _ in range(n)]  # i 선수와의 순위가 결정된 선수들
+    
+    for player in range(n):
+        # lose map 순회
+        stack = [player]
+        while stack:
+            j = stack.pop()
+            for k in lose_map[j]:
+                if k not in determined_map[player]:
+                    determined_map[player].add(k)
+                    stack.append(k)
+        # win map 순회
+        stack = [player]
+        while stack:
+            j = stack.pop()
+            for k in win_map[j]:
+                if k not in determined_map[player]:
+                    determined_map[player].add(k)
+                    stack.append(k)
+    
+    count = 0
+    
+    for player_set in determined_map:
+        if len(player_set) == n - 1: count += 1
+    return count
