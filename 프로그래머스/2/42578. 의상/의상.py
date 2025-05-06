f8b5a82f007c40841d7310112239ffad9f77578a
chointer <chointer04@gmail.com>
2025-05-07 00:01:19 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,30 @@
-from collections import defaultdict
+# N <= 30
+# 시간 초과가 하나 발생했다. combinations를 반복해서 사용했는데, 안입는 경우를 추가해서 경우의 수를 구하면 간단해진다...
+
+from collections import Counter
 
 def solution(clothes):
-    clothes_dict = defaultdict(int)
-    for c in clothes:
-        clothes_dict[c[1]] += 1
+    counter = Counter([cloth[1] for cloth in clothes])
+    
+    count = 1
+    
+    for i in counter.values():
+        count *= i + 1
+            
+    return count - 1
+    
+    
+"""def solution(clothes):
+    clo_dict = {}
+    for name, clo in clothes:
+        if clo in clo_dict:
+            clo_dict[clo] += 1
+        else:
+            clo_dict[clo] = 1
     
     answer = 1
-    for v in clothes_dict.values():
-        answer *= (v + 1)
+    for key in clo_dict.keys():
+        answer *= (clo_dict[key] + 1)
         
     return answer - 1
+"""
