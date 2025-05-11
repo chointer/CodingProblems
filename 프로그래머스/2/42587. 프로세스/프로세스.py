179dabfc0571faa65cbe3e6cfa871b9521111784
chointer <chointer04@gmail.com>
2025-05-11 20:39:48 +0900

    [level 2] Title: 프로세스, Time: 0.25 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,30 +1,55 @@
-# priorities <= 1e2
-# 그냥 구현?
-from collections import deque, Counter
+# N <= 1e2
+# 단순히 순서대로 체크하면 N**2 <= 1e4  * 10(=rank 개수)
+# priorities에서 target을 기록할 자료형으로 바꾼다.
+# target이 지워질 때까지 체크한다.
+
+from collections import Counter, deque
 
 def solution(priorities, location):
-    # sort map: [[priority, n], ...]
-    counter = Counter(priorities)
-    sort_map = []
-    for p, n in counter.items():
-        sort_map.append([p, n])
-    sort_map.sort(reverse=True)
-    sort_map = deque(sort_map)
-    
+    counts = Counter(priorities)
     priorities = deque([(p, i) for i, p in enumerate(priorities)])
-    goal_process = (priorities[location])
+    dones = 0
     
-    # process managing
-    order = 1
     while priorities:
-        pop = priorities.popleft()
-        if pop[0] < sort_map[0][0]:
-            priorities.append(pop)
-        elif pop[0] == sort_map[0][0]:
-            sort_map[0][1] -= 1
-            if pop == goal_process:
-                return order
-            if sort_map[0][1] == 0:
-                sort_map.popleft()
-            order += 1
+        rank, idx = priorities.popleft()
+
+        if rank == max(counts.keys()):
+            dones += 1
+            counts[rank] -= 1
+            if counts[rank] == 0:
+                del counts[rank]
+            if idx == location:
+                return dones     
+            
+        else:
+            priorities.append((rank, idx))
+        
     return -1
+
+
+
+
+
+
+
+"""
+def solution(priorities, location):
+    loc = [False] * len(priorities)
+    loc[location] = True
+    order = 0
+    
+    while priorities:
+        idx_max = priorities.index(max(priorities))
+        
+        if idx_max != 0:
+            priorities = priorities[idx_max:] + priorities[:idx_max]
+            loc = loc[idx_max:] + loc[:idx_max]
+        
+        if loc[0] == True: break
+        
+        priorities.pop(0)
+        loc.pop(0)
+        order += 1
+            
+    return order + 1
+"""
