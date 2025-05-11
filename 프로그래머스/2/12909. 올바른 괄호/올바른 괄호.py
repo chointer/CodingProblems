27cb8139ee9dc0371e2a7055a2e2f714ed735b61
chointer <chointer04@gmail.com>
2025-05-11 20:19:37 +0900

    [level 2] Title: 올바른 괄호, Time: 7.33 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,40 @@
-# N < 1e6
+# s <= 1e5
+# 1. '('가 +1, ')' -1일 때, 음수가 되면 안된다.
+# 2. 확인이 끝난 뒤에 0이 아니면 안된다.
+
 def solution(s):
-    answer = True
     count = 0
     for i in s:
         if i == '(':
             count += 1
         else:
             count -= 1
-            if count < 0:
-                answer = False
-                break
+        
+        if count < 0:
+            return False
+        
+    return False if count != 0 else True
+            
+        
     
-    if count > 0:
-        answer = False
     
-    return answer
+    
+    
+    
+    
+
+"""
+def solution(s):
+    l = 0
+    for i in s:
+        if i == '(':
+            l += 1
+        else:
+            l -= 1
+        if l < 0:
+            return False
+    if l == 0:
+        return True
+    else:
+        return False
+"""
