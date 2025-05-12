d215b81711734ff98e6c1e20bd26e9ab93f48473
chointer <chointer04@gmail.com>
2025-05-13 00:47:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.53 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,39 +1,62 @@
-# N <= 500.
-# 항상 짧은 것 먼저하는 것이 낫다.
-# 2개의 요청이 대기중 일 때, 
-# 각 요청의 지금까지의 대기 시간과 수행 시간을 각각 x0 + x > y0 + y 이라 하면,
-# x 후 y: x0 + x + y0 + x + y
-# y 후 x: x0 + y + x + y0 + y
-# 비교하면 1번째 방법이 y - x 만큼 더 빠르다.
+import heapq as hq
+from collections import deque
 
+# N <= 500
+# 우선 순위: (소요 시간, 요청 시각, 작업 번호)
+# t 시점,
+# 1. t 시점까지의 jobs(deque)에서 job을 대기열(heapq)에 추가한다.
+# 2. 최우선 작업을 대기열에서 하나 꺼내서 수행한다.
+#   2-1. 대기열에 작업이 없으면, jobs[0]의 시점으로 t를 갱신한다.
+# 3. 수행한 작업 반환 시간을 계산하고, 끝난 시각으로 t를 갱신한다.
+
+def solution(jobs):
+    turntimes = []
+    waits = []
+    jobs = [(l, s, i) for i, (s, l) in enumerate(jobs)]
+    jobs = sorted(jobs, key=lambda x: x[1])
+    jobs = deque(jobs)
+    t = 0
+
+    while jobs or waits:
+        while jobs and jobs[0][1] <= t:
+            hq.heappush(waits, jobs.popleft())
+
+        if waits:
+            length, start, _ = hq.heappop(waits)
+            t += length
+            turntimes.append(t - start)
+        else:
+            t = jobs[0][1]
+
+    return sum(turntimes)//len(turntimes)
+    
+
+"""
 import heapq
 from collections import deque
 
 def solution(jobs):
+    q = []                  # delayed works
     N = len(jobs)
-    jobs = deque(sorted(jobs))
-    tasks = []
-    t_done = 0
+    jobs.sort()
+    jobs = deque(jobs)
+    print(jobs)
+    
+    t = 0
     t_work = 0
-    
-    while jobs or tasks:
-        if not jobs and not tasks:
-            break
-            
-        # 작업이 없으면 무조건 다음 작업 받기
-        if not tasks:
-            t0, t = jobs.popleft()
-            heapq.heappush(tasks, (t, t0))
-            t_done = t0
+    while jobs or q:
+        if q:
+            work = heapq.heappop(q)
+            work = (work[1], work[0])
+        else:
+            work = jobs.popleft()
+            t = work[0]
         
-        # 작업 선택 수행
-        t, t0 = heapq.heappop(tasks)
-        t_done += t
-        t_work += t_done - t0
-            
-        # 현 작업을 마칠 때까지 job 받기
-        while jobs and jobs[0][0] <= t_done:
-            t0, t = jobs.popleft()
-            heapq.heappush(tasks, (t, t0))
-    
+        t += work[1]
+        t_work += t - work[0]
+        
+        while jobs and jobs[0][0] < t:
+            delay = jobs.popleft()
+            heapq.heappush(q, (delay[1], delay[0]))
     return t_work//N
+"""
