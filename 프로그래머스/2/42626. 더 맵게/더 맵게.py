b711c05b6b4d93754d8cd2516364d243a5d86cd5
chointer <chointer04@gmail.com>
2025-05-12 23:46:22 +0900

    [level 2] Title: 더 맵게, Time: 1670.35 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,39 @@
-# N <= 1e6
-# heapq로 가장 작은 음식 두 개 pop
+# N <= 1e6, K <= 1e9, el <= 1e6
+# 두 음식을 섞으면, 총 음식 수가 1개 감소한다. -> 최대 반복 가능 횟수 = 1e6
 
-import heapq
+import heapq as hq
+
 def solution(scoville, K):
-    heapq.heapify(scoville)
+    hq.heapify(scoville)
     
     count_mix = 0
-    while scoville[0] < K and len(scoville) > 1:
-        a = heapq.heappop(scoville)
-        b = heapq.heappop(scoville)
-        heapq.heappush(scoville, a + b * 2)
+    while len(scoville) > 1 and scoville[0] < K:
+        min0 = hq.heappop(scoville)
+        min1 = hq.heappop(scoville)
+        hq.heappush(scoville, min0 + 2 * min1)
         count_mix += 1
     
+    if scoville and scoville[0] < K:
+        return -1
+    else:
+        return count_mix
+
+"""
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
     
-    return count_mix if scoville[0] >= K else -1
+    while scoville[0] < K:
+        if len(scoville) == 1:
+            answer = -1
+            break
+            
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + b*2)
+        answer += 1
+        
+    return answer
+"""
