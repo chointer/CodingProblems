dea536f4e7c44d8a5d0551e5c2d18e8788f9cc9c
chointer <chointer04@gmail.com>
2025-05-13 23:15:48 +0900

    [level 2] Title: 가장 큰 수, Time: 344.58 ms, Memory: 22.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,18 +1,36 @@
+# N <= 1000
 from functools import cmp_to_key
 
-def func(x, y):
-    xy = int(x + y)
-    yx = int(y + x)
-    if xy >= yx:
+def cmp(a, b):
+    ab = a + b
+    ba = b + a
+    
+    if ab > ba:
         return -1
-    else:
+    elif ab < ba:
         return 1
+    else:
+        return 0
     
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=cmp_to_key(func))
+    numbers.sort(key=cmp_to_key(cmp))
     
-    if numbers[0] == '0':
-        return "0"
+    answer = "".join(numbers)
     
-    return "".join(numbers)
+    return "0" if answer[0] == '0' else answer
+    
+    
+
+
+'''
+def solution(numbers):
+    # N = 1
+    if len(numbers) == 1:
+        return str(numbers[0])
+    
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x: x * 4, reverse=True)
+
+    return str(int("".join(numbers)))
+'''
